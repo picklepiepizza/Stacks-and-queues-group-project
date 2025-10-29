62c866bc2a27c3a4fe1afe65a27f2e703b0b81f7
Super <Hamid82420@gmail.com>
2025-10-29 12:17:00 -0400

    Add Stack class definition with basic methods

--- Stack.h
+++ Stack.h
@@ -1,1 +1,15 @@
+//Stack.h
 
+#ifndef STACK_H
+#define STACK_H
+
+#include "LinkedList.h"
+
+class Stack : public LinkedList {
+    void push (int val);
+    void pop();
+    void display() override;
+
+};
+
+#endif
