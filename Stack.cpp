e0088e5b5a43e1be1401d21aa6d3acba7ebb4a63
Super <Hamid82420@gmail.com>
2025-10-29 12:19:34 -0400

    Implement push method for Stack class

--- Stack.cpp
+++ Stack.cpp
@@ -1,1 +1,14 @@
+//Stack.cpp
 
+#include "Stack.h"
+
+void Stack::push(int val) {
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+
+    if (!tail) {
+        tail = newNode;
+    }
+    cout << "Pushed: " << val << endl;
+}
