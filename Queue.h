f6e9c4967cf95ed62616a2b1d46279167a3d767d
Super <Hamid82420@gmail.com>
2025-10-29 00:00:14 -0400

    Define Queue class with enqueue, dequeue, and display

--- Queue.h
+++ Queue.h
@@ -1,1 +1,16 @@
+//Queue.h
 
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "LinkedList.h"
+
+class Queue : public LinkedList {
+public:
+    void enqueue(int val);
+    void dequeue();
+    void display() override;
+};
+
+
+#endif
