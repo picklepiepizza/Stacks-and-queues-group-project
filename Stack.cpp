ed7d3ac6265ec1a781e5c03746f493a6fca7be1e
Super <Hamid82420@gmail.com>
2025-10-29 12:23:44 -0400

    Implement pop and display methods for Stack

--- Stack.cpp
+++ Stack.cpp
@@ -12,3 +12,29 @@ void Stack::push(int val) {
     }
     cout << "Pushed: " << val << endl;
 }
+
+void Stack::pop() {
+    if (!head) {     
+        cout << "Stack is empty!\n";
+        return;
+    }
+
+    Node* temp = head;
+    cout << "Popped: " << temp->data << endl;
+    head = head->next;
+    delete temp;
+
+    if (!head) {
+        tail = nullptr;
+    }
+}
+
+void Stack::display() {
+    cout << "Stack (Top to Bottom): ";
+    Node* temp = head;
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
