550c2bdfb46bc215a80fb395fe094474d8828f67
Super <Hamid82420@gmail.com>
2025-10-30 14:32:50 -0400

    Refactor Stack class method comments for clarity

--- Stack.h
+++ Stack.h
@@ -5,11 +5,12 @@
 
 #include "LinkedList.h"
 
+//Stack inherits from LinkedList (it uses the same 'head' and 'tail' pointers)
 class Stack : public LinkedList {
 public:
-    void push (int val);
-    void pop();
-    void display() override;
+    void push (int val);           //Add new element to the top (head)
+    void pop();                    //Remove element from the top
+    void display() override;       //Prints all elements from top to bottom
 
 };
 
