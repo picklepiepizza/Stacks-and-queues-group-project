b117db77dc087313dde5d5b44f75774e1f8a212d
Super <Hamid82420@gmail.com>
2025-10-30 14:33:17 -0400

    Refactor Stack methods for improved clarity
    Added comments

--- Stack.cpp
+++ Stack.cpp
@@ -2,37 +2,41 @@
 
 #include "Stack.h"
 
+//Push adds a new node at the beginning (LIFO) order
 void Stack::push(int val) {
     Node* newNode = new Node(val);
     newNode->next = head;
-    head = newNode;
+    head = newNode;                        //Move head pointer to new Node (new top)
 
-    if (!tail) {
+    if (!tail) {                        //If the list is empty, set tail to this new node
         tail = newNode;
     }
     cout << "Pushed: " << val << endl;
 }
 
+//Pop removes the top node (head)
 void Stack::pop() {
-    if (!head) {     
+    if (!head) {                          //if head == nullptr then stack is empty
         cout << "Stack is empty!\n";
         return;
     }
 
     Node* temp = head;
     cout << "Popped: " << temp->data << endl;
-    head = head->next;
+    head = head->next;                        //Moves head pointer to the next node
     delete temp;
 
-    if (!head) {
+    if (!head) {                         //if the stack is now empty, reset tail as well
         tail = nullptr;
     }
 }
 
+//Display all nodes (top to bottom)
 void Stack::display() {
     cout << "Stack (Top to Bottom): ";
     Node* temp = head;
-    while (temp) {
+
+    while (temp) {                   //Traverse until end (nullptr) is reached
         cout << temp->data << " ";
         temp = temp->next;
     }
