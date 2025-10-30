ae11b52de57d1e8f7b98b3a77d150215ea1540a3
Super <Hamid82420@gmail.com>
2025-10-30 14:37:17 -0400

    Update main.cpp
    Added comments. The code runs fine on my device

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 int main() {
-    Stack stack;
-    Queue queue;
-    int choice, val;
+    Stack stack;          //Created a stack object
+    Queue queue;          //Created a queue object
+    int choice, val; 
 
     do {
         cout << "\n========== Menu ==========\n" 
@@ -29,29 +29,31 @@ int main() {
             case 1:
                 cout << "Enter value to push: ";
                 cin >> val;
-                stack.push(val);
+                stack.push(val);             //Adds val to the top of the stack
                 break;
-            case 2:
-                stack.pop();
+            case 2: 
+                stack.pop();                 //Removes top value from stack
                 break;
             case 3:
                 cout << "Enter a value to enqueue: ";
                 cin >> val;
-                queue.enqueue(val);
+                queue.enqueue(val);          //Add value to end of queue
                 break;
             case 4: 
-                queue.dequeue();
+                queue.dequeue();            //Remove fron value from queue
                 break;
             case 5:
-                stack.display();
+                stack.display();            //Shows contents of stack
                 break;
             case 6: 
-                queue.display();
+                queue.display();            //Shows contents of queue
                 break;
-            case 7: {
+            case 7: {                                 //Added braces to create the local scope for the "it" variable
                 cout << "Iterating through Stack (Top to Bottom): ";
-                Iterator it(stack.getHead());
-                while (it.hasNext()) {
+
+                Iterator it(stack.getHead());      //Iterator starts at head
+
+                while (it.hasNext()) {          //While iterator has not reached "nullptr", there are still more nodes to visit
                     cout << it.next() << " ";
                 }
             }
@@ -72,7 +74,7 @@ int main() {
             default:
                 cout << "Invalid Choice!!\n";
         }
-    } while (choice != 9);
+    } while (choice != 9);                //Continue untill user exits by entering 9
 
     return 0;
 }
