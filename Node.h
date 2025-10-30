000bfd86e0655cb59820febd1f7a9d60ae662106
Super <Hamid82420@gmail.com>
2025-10-30 14:36:33 -0400

    Refactor Node class to struct
    Refactored Node class to a simpler struct with data and next pointers.

--- Node.h
+++ Node.h
@@ -1,19 +1,7 @@
 #ifndef NODE_H
 #define NODE_H
 
-class Node {
-   private:
-      Node* node;
-      int payload;
-   public:
-      Node();
-      Node* getNext() const;
-      int getPayload() const;
-      void setNext(Node* next);
-      void setPayload(int payload);
-};
 
-//This is my version of Node
 struct Node {
     int data;
     Node* next;
