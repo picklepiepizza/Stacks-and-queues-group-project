1faabeb08effb5da36c16968229ba6db157fc45e
Super <Hamid82420@gmail.com>
2025-10-30 14:33:57 -0400

    Refactor Queue class method comments
    Added comments

--- Queue.h
+++ Queue.h
@@ -5,11 +5,12 @@
 
 #include "LinkedList.h"
 
+//Queue inherits from LinkedList (same head/tail but FIFO order)
 class Queue : public LinkedList {
 public:
-    void enqueue(int val);
-    void dequeue();
-    void display() override;
+    void enqueue(int val);                //Add new element at the head
+    void dequeue();                       //Remove element from the front
+    void display() override;             //Prints elements from front to back
 };
 
 
