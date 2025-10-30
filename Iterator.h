dbf9b18bf9d9ce7d844bdee847ad35c3c486191d
Super <Hamid82420@gmail.com>
2025-10-30 14:35:24 -0400

    Fix typo in hasNext method comment

--- Iterator.h
+++ Iterator.h
@@ -12,7 +12,7 @@ private:
 public:
     Iterator(Node* start);          //Constructor tthat starts at the head node
 
-    bool hasNext();            //Retunds true if there is another node to visit
+    bool hasNext();            //Returns true if there is another node to visit
     int next();                //Returns current node's data and moves to the next node
 };
 
